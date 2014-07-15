f8eee8d472f624950771d0533c4fc8327fb21e19
Kevin Chan <chessmasterhong@yahoo.com>
2014-07-15 15:57:57 -0400

    Added extension to GitHub API rate limiting.

--- scripts/get_repos.js
+++ scripts/get_repos.js
@@ -1,2 +1,2 @@
-var data = httpGet('https://api.github.com/users/chessmasterhong/repos');
+var data = httpGet('https://api.github.com/users/chessmasterhong/repos?client_id=' + CLIENT.ID + '&client_secret=' + CLIENT.SECRET);
 document.write(data);
