187430c373982220dc3952274886dfd81a0b2ae2
Kevin Chan <chessmasterhong@yahoo.com>
2014-07-15 16:13:12 -0400

    Refactored username from API URL.

--- scripts/get_repos.js
+++ scripts/get_repos.js
@@ -1,2 +1,2 @@
-var data = httpGet(GITHUB_API_BASE_URL + '/repos?client_id=' + CLIENT.ID + '&client_secret=' + CLIENT.SECRET);
-document.write(data);
+var data = httpGet(GITHUB_API_BASE_URL + '/' + USERNAME + '/repos?client_id=' + CLIENT.ID + '&client_secret=' + CLIENT.SECRET);
+document.write('<pre>' + data + '</pre>');
