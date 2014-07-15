4a873492f6e5505e90c0733f21bad0235486bbd9
Kevin Chan <chessmasterhong@yahoo.com>
2014-07-15 16:04:23 -0400

    Refactored base URL.

--- scripts/get_repos.js
+++ scripts/get_repos.js
@@ -1,2 +1,2 @@
-var data = httpGet('https://api.github.com/users/chessmasterhong/repos?client_id=' + CLIENT.ID + '&client_secret=' + CLIENT.SECRET);
+var data = httpGet(GITHUB_API_BASE_URL + '/repos?client_id=' + CLIENT.ID + '&client_secret=' + CLIENT.SECRET);
 document.write(data);
