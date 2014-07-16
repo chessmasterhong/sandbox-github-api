6f34ed981c22c0fafeea43d17ed8b4b0a568b1ff
Kevin Chan <chessmasterhong@yahoo.com>
2014-07-16 00:46:37 -0400

    Formatted output for get comments script.

--- scripts/get_comments.js
+++ scripts/get_comments.js
@@ -1,1 +1,7 @@
-document.write('<pre>' + httpGet(GITHUB_API_BASE_URL + '/repos/' + USERNAME + '/WaterEmblem/comments?client_id=' + CLIENT.ID + '&client_secret=' + CLIENT.SECRET, false) + '</pre>');
+var REPONAME = 'WaterEmblem';
+
+var data = httpGet(GITHUB_API_BASE_URL + '/repos/' + USERNAME + '/' + REPONAME + '/comments?client_id=' + CLIENT.ID + '&client_secret=' + CLIENT.SECRET);
+
+for(var repo_index = 0; repo_index < data.length; repo_index++) {
+    document.write('<p><strong>' + data[repo_index].user.login + '</strong>: ' + data[repo_index].body + '</p>');
+}
