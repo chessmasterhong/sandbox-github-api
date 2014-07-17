3d9e17f573ffe1e1f83a665b3356fe4ab9a5dc58
Kevin Chan <chessmasterhong@yahoo.com>
2014-07-16 22:32:15 -0400

    Updated get commits script to get commits for all contributors.

--- scripts/get_commits.js
+++ scripts/get_commits.js
@@ -1,7 +1,7 @@
 var REPO_NAME = 'WaterEmblem';
 
-var FILTER_USER = 'chessmasterhong';
+var data = httpGet(GITHUB_API_BASE_URL + '/repos/' + REPO_OWNER + '/' + REPO_NAME + '/stats/contributors?client_id=' + CLIENT.ID + '&client_secret=' + CLIENT.SECRET);
 
-var data = httpGet(GITHUB_API_BASE_URL + '/repos/' + REPO_OWNER + '/' + REPO_NAME + '/commits?author=' + FILTER_USER + '&client_id=' + CLIENT.ID + '&client_secret=' + CLIENT.SECRET);
-
-document.write(FILTER_USER + ' has made ' + data.length + ' commits to ' + REPO_NAME);
+for(var contributor_index = 0; contributor_index < data.length; contributor_index++) {
+    document.write(data[contributor_index].author.login + ': ' + data[contributor_index].total + '<br>');
+}
