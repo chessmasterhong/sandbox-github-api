cc9f7db2e618cb92c260312835ddf18c9b8db09e
Kevin Chan <chessmasterhong@yahoo.com>
2014-07-16 21:27:36 -0400

    Modified get issues script to get issue count opened by each contributor.

--- scripts/get_issues.js
+++ scripts/get_issues.js
@@ -1,7 +1,23 @@
 var REPO_NAME = 'WaterEmblem';
 
-var data = httpGet(GITHUB_API_BASE_URL + '/repos/' + REPO_OWNER + '/' + REPO_NAME + '/issues?client_id=' + CLIENT.ID + '&client_secret=' + CLIENT.SECRET);
+var contributors = getContributors(REPO_NAME, CLIENT.ID, CLIENT.SECRET);
 
-for(var repo_index = 0; repo_index < data.length; repo_index++) {
-    document.write('<p><strong>Issue #' + data[repo_index].number + '</strong>: ' + data[repo_index].title + '<br>' + data[repo_index].body + '</p>');
+var contributor_data = [];
+
+for(var contributor_index = 0; contributor_index < contributors.length; contributor_index++) {
+    contributor_data[contributor_index] = {
+        "name": contributors[contributor_index],
+        "issue_count": 0
+    }
+}
+
+var data = [];
+
+for(var contributor_index = 0; contributor_index < contributors.length; contributor_index++) {
+     data.push(httpGet(GITHUB_API_BASE_URL + '/repos/' + REPO_OWNER + '/' + REPO_NAME + '/issues?creator=' + contributors[contributor_index] + '&client_id=' + CLIENT.ID + '&client_secret=' + CLIENT.SECRET));
+}
+
+for(var contributor_index = 0; contributor_index < contributors.length; contributor_index++) {
+    contributor_data[contributor_index].issue_count = data[contributor_index].length;
+    document.write(contributor_data[contributor_index].name + ': ' + contributor_data[contributor_index].issue_count + '<br>');
 }
