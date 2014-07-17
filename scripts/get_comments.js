a9cf8ec59e765f77d75715374e882381f220286e
Kevin Chan <chessmasterhong@yahoo.com>
2014-07-17 01:07:48 -0400

    Updated get comments script with get pull request comment count by contributor.

--- scripts/get_comments.js
+++ scripts/get_comments.js
@@ -1,4 +1,4 @@
-// Get total commit comments and total issue comments made by each contributor
+// Get total commit comment count, total issue comment count, and total pull request comment count made by each contributor
 
 var REPO_NAME = 'WaterEmblem';
 
@@ -10,7 +10,8 @@ for(var contributor_index = 0; contributor_index < contributors.length; contribu
     contributor_comments[contributor_index] = {
         'name': contributors[contributor_index],
         'commit_comments': 0,
-        'issue_comments': 0
+        'issue_comments': 0,
+        'pull_comments': 0
     }
 }
 
@@ -26,24 +27,22 @@ for(var comment_index = 0; comment_index < data_commits.length; comment_index++)
 
 var data_issues = httpGet(GITHUB_API_BASE_URL + '/repos/' + REPO_OWNER + '/' + REPO_NAME + '/issues?state=all&client_id=' + CLIENT.ID + '&client_secret=' + CLIENT.SECRET);
 
-var issue_numbers = [];
-
 for(var issue_index = 0; issue_index < data_issues.length; issue_index++) {
     var issue_comments = httpGet(GITHUB_API_BASE_URL + '/repos/' + REPO_OWNER + '/' + REPO_NAME + '/issues/' + data_issues[issue_index].number + '/comments?client_id=' + CLIENT.ID + '&client_secret=' + CLIENT.SECRET);
 
     for(var comment_index = 0; comment_index < issue_comments.length; comment_index++) {
         for(var contributor_index = 0; contributor_index < contributors.length; contributor_index++) {
             if(issue_comments[comment_index].user.login === contributor_comments[contributor_index].name) {
-                contributor_comments[contributor_index].issue_comments++;
+                if(data_issues[issue_index].pull_request) {
+                    contributor_comments[contributor_index].pull_comments++;
+                } else {
+                    contributor_comments[contributor_index].issue_comments++;
+                }
             }
         }
     }
 }
 
 for(var c = 0; c < contributors.length; c++) {
-    document.write(contributor_comments[c].name + ': ' + contributor_comments[c].commit_comments + ' comments commits, ' + contributor_comments[c].issue_comments + ' issue comments<br>');
+    document.write(contributor_comments[c].name + ': ' + contributor_comments[c].commit_comments + ' comments commits, ' + contributor_comments[c].issue_comments + ' issue comments, '  + contributor_comments[c].pull_comments + ' pull request comments<br>');
 }
-
-//for(var comment_index = 0; comment_index < data_commits.length; comment_index++) {
-//    document.write('<p><strong>' + data_commits[comment_index].user.login + '</strong>: ' + data_commits[comment_index].body + '</p>');
-//}
