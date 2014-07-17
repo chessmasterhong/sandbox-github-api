9a3f283a22cac380f7228753c3ca4ceea2d3008d
Kevin Chan <chessmasterhong@yahoo.com>
2014-07-16 20:30:04 -0400

    Renamed variable names for consistency with API.

--- scripts/get_comments.js
+++ scripts/get_comments.js
@@ -1,6 +1,6 @@
-var REPONAME = 'WaterEmblem';
+var REPO_NAME = 'WaterEmblem';
 
-var data = httpGet(GITHUB_API_BASE_URL + '/repos/' + USERNAME + '/' + REPONAME + '/comments?client_id=' + CLIENT.ID + '&client_secret=' + CLIENT.SECRET);
+var data = httpGet(GITHUB_API_BASE_URL + '/repos/' + REPO_OWNER + '/' + REPO_NAME + '/comments?client_id=' + CLIENT.ID + '&client_secret=' + CLIENT.SECRET);
 
 for(var repo_index = 0; repo_index < data.length; repo_index++) {
     document.write('<p><strong>' + data[repo_index].user.login + '</strong>: ' + data[repo_index].body + '</p>');
--- scripts/get_issues.js
+++ scripts/get_issues.js
@@ -1,6 +1,6 @@
-var REPONAME = 'WaterEmblem';
+var REPO_NAME = 'WaterEmblem';
 
-var data = httpGet(GITHUB_API_BASE_URL + '/repos/' + USERNAME + '/' + REPONAME + '/issues?client_id=' + CLIENT.ID + '&client_secret=' + CLIENT.SECRET);
+var data = httpGet(GITHUB_API_BASE_URL + '/repos/' + REPO_OWNER + '/' + REPO_NAME + '/issues?client_id=' + CLIENT.ID + '&client_secret=' + CLIENT.SECRET);
 
 for(var repo_index = 0; repo_index < data.length; repo_index++) {
     document.write('<p><strong>Issue #' + data[repo_index].number + '</strong>: ' + data[repo_index].title + '<br>' + data[repo_index].body + '</p>');
