efabc109dffccc17d05eecc583747accaa0236d7
Kevin Chan <chessmasterhong@yahoo.com>
2014-07-16 21:59:51 -0400

    Optimized data structure to reduce API calls.

--- scripts/get_issues.js
+++ scripts/get_issues.js
@@ -6,18 +6,22 @@ var contributor_data = [];
 
 for(var contributor_index = 0; contributor_index < contributors.length; contributor_index++) {
     contributor_data[contributor_index] = {
-        "name": contributors[contributor_index],
-        "issue_count": 0
+        'name': contributors[contributor_index],
+        'issue_count': 0
     }
 }
 
-var data = [];
+var data = httpGet(GITHUB_API_BASE_URL + '/repos/' + REPO_OWNER + '/' + REPO_NAME + '/issues?state=all&client_id=' + CLIENT.ID + '&client_secret=' + CLIENT.SECRET);
 
-for(var contributor_index = 0; contributor_index < contributors.length; contributor_index++) {
-     data.push(httpGet(GITHUB_API_BASE_URL + '/repos/' + REPO_OWNER + '/' + REPO_NAME + '/issues?creator=' + contributors[contributor_index] + '&client_id=' + CLIENT.ID + '&client_secret=' + CLIENT.SECRET));
+for(var issue_index = 0; issue_index < data.length; issue_index++) {
+    for(var contributor_index = 0; contributor_index < contributors.length; contributor_index++) {
+        if(data[issue_index].user.login === contributor_data[contributor_index].name) {
+            contributor_data[contributor_index].issue_count++;
+            break;
+        }
+    }
 }
 
-for(var contributor_index = 0; contributor_index < contributors.length; contributor_index++) {
-    contributor_data[contributor_index].issue_count = data[contributor_index].length;
-    document.write(contributor_data[contributor_index].name + ': ' + contributor_data[contributor_index].issue_count + '<br>');
+for(var c = 0; c < contributors.length; c++) {
+    document.write(contributor_data[c].name + ': ' + contributor_data[c].issue_count + '<br>');
 }
