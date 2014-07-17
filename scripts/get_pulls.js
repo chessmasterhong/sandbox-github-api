b90a416a738ed479106774464e6ba2d64f3205f5
Kevin Chan <chessmasterhong@yahoo.com>
2014-07-17 00:01:16 -0400

    Added get pull requests by contributor script.

---
+++ scripts/get_pulls.js
@@ -0,0 +1,24 @@
+// Get total pull requests opened by each contributor
+
+var REPO_NAME = 'WaterEmblem';
+
+var contributors = getContributors(REPO_NAME, CLIENT.ID, CLIENT.SECRET);
+
+var contributor_pulls = [];
+
+for(var contributor_index = 0; contributor_index < contributors.length; contributor_index++) {
+    contributor_pulls[contributor_index] = 0;
+}
+
+var data = httpGet(GITHUB_API_BASE_URL + '/repos/' + REPO_OWNER + '/' + REPO_NAME + '/pulls?state=all&client_id=' + CLIENT.ID + '&client_secret=' + CLIENT.SECRET);
+
+for(var pull_index = 0; pull_index < data.length; pull_index++) {
+    var i = contributors.indexOf(data[pull_index].user.login);
+    if(i >= 0) {
+        contributor_pulls[i]++;
+    }
+}
+
+for(var contributor_index = 0; contributor_index < contributors.length; contributor_index++) {
+    document.write(contributors[contributor_index] + ': ' + contributor_pulls[contributor_index] + '<br>');
+}
