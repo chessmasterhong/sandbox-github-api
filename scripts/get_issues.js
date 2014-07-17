bc9cf1b0657ffeeb556ddfbfa372f6c676073896
Kevin Chan <chessmasterhong@yahoo.com>
2014-07-16 22:08:57 -0400

    Added get closed issue count by user in repository.

--- scripts/get_issues.js
+++ scripts/get_issues.js
@@ -1,4 +1,4 @@
-var REPO_NAME = 'WaterEmblem';
+var REPO_NAME = 'impact-atmosphere';
 
 var contributors = getContributors(REPO_NAME, CLIENT.ID, CLIENT.SECRET);
 
@@ -7,7 +7,8 @@ var contributor_data = [];
 for(var contributor_index = 0; contributor_index < contributors.length; contributor_index++) {
     contributor_data[contributor_index] = {
         'name': contributors[contributor_index],
-        'issue_count': 0
+        'issues_opened': 0,
+        'issues_assigned': 0
     }
 }
 
@@ -16,12 +17,15 @@ var data = httpGet(GITHUB_API_BASE_URL + '/repos/' + REPO_OWNER + '/' + REPO_NAM
 for(var issue_index = 0; issue_index < data.length; issue_index++) {
     for(var contributor_index = 0; contributor_index < contributors.length; contributor_index++) {
         if(data[issue_index].user.login === contributor_data[contributor_index].name) {
-            contributor_data[contributor_index].issue_count++;
-            break;
+            contributor_data[contributor_index].issues_opened++;
+        }
+
+        if(data[issue_index].assignee && data[issue_index].assignee.login === contributor_data[contributor_index].name) {
+            contributor_data[contributor_index].issues_assigned++;
         }
     }
 }
 
 for(var c = 0; c < contributors.length; c++) {
-    document.write(contributor_data[c].name + ': ' + contributor_data[c].issue_count + '<br>');
+    document.write(contributor_data[c].name + ': ' + contributor_data[c].issues_opened + ' opened total, ' + contributor_data[c].issues_assigned + ' assigned total<br>');
 }
