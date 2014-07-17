0182399eae01ce559087403dd3480c585a7d88e5
Kevin Chan <chessmasterhong@yahoo.com>
2014-07-17 00:34:45 -0400

    Updated get comments to tally sum of commit comments by contributors. Minor variable rename in get_issues.js for consistency.

--- scripts/get_comments.js
+++ scripts/get_comments.js
@@ -1,6 +1,31 @@
+// Get total commit comments made by each contributor
+
 var REPO_NAME = 'WaterEmblem';
 
+var contributors = getContributors(REPO_NAME, CLIENT.ID, CLIENT.SECRET);
+
+var contributor_comments = [];
+
+for(var contributor_index = 0; contributor_index < contributors.length; contributor_index++) {
+    contributor_comments[contributor_index] = {
+        'name': contributors[contributor_index],
+        'commit_comments': 0
+    }
+}
+
 var data = httpGet(GITHUB_API_BASE_URL + '/repos/' + REPO_OWNER + '/' + REPO_NAME + '/comments?client_id=' + CLIENT.ID + '&client_secret=' + CLIENT.SECRET);
+
+for(var comment_index = 0; comment_index < data.length; comment_index++) {
+    for(var contributor_index = 0; contributor_index < contributors.length; contributor_index++) {
+        if(data[comment_index].user.login === contributor_comments[contributor_index].name) {
+            contributor_comments[contributor_index].commit_comments++;
+        }
+    }
+}
+
+for(var c = 0; c < contributors.length; c++) {
+    document.write(contributor_comments[c].name + ': ' + contributor_comments[c].commit_comments + ' comments made on commits<br>');
+}
 
 for(var comment_index = 0; comment_index < data.length; comment_index++) {
     document.write('<p><strong>' + data[comment_index].user.login + '</strong>: ' + data[comment_index].body + '</p>');
--- scripts/get_issues.js
+++ scripts/get_issues.js
@@ -4,10 +4,10 @@ var REPO_NAME = 'WaterEmblem';
 
 var contributors = getContributors(REPO_NAME, CLIENT.ID, CLIENT.SECRET);
 
-var contributor_data = [];
+var contributor_issues = [];
 
 for(var contributor_index = 0; contributor_index < contributors.length; contributor_index++) {
-    contributor_data[contributor_index] = {
+    contributor_issues[contributor_index] = {
         'name': contributors[contributor_index],
         'issues_opened': 0,
         'issues_assigned': 0
@@ -18,18 +18,18 @@ var data = httpGet(GITHUB_API_BASE_URL + '/repos/' + REPO_OWNER + '/' + REPO_NAM
 
 for(var issue_index = 0; issue_index < data.length; issue_index++) {
     for(var contributor_index = 0; contributor_index < contributors.length; contributor_index++) {
-        if(data[issue_index].user.login === contributor_data[contributor_index].name) {
-            contributor_data[contributor_index].issues_opened++;
+        if(data[issue_index].user.login === contributor_issues[contributor_index].name) {
+            contributor_issues[contributor_index].issues_opened++;
         }
 
-        if(data[issue_index].assignee && data[issue_index].assignee.login === contributor_data[contributor_index].name) {
-            contributor_data[contributor_index].issues_assigned++;
+        if(data[issue_index].assignee && data[issue_index].assignee.login === contributor_issues[contributor_index].name) {
+            contributor_issues[contributor_index].issues_assigned++;
         }
     }
 }
 
 for(var c = 0; c < contributors.length; c++) {
-    document.write(contributor_data[c].name + ': ' + contributor_data[c].issues_opened + ' opened total, ' + contributor_data[c].issues_assigned + ' assigned total<br>');
+    document.write(contributor_issues[c].name + ': ' + contributor_issues[c].issues_opened + ' opened total, ' + contributor_issues[c].issues_assigned + ' assigned total<br>');
 }
 
 for(var repo_index = 0; repo_index < data.length; repo_index++) {
