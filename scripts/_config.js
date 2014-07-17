dbd4ff0d3885348ea44987a3fb993da46541e252
Kevin Chan <chessmasterhong@yahoo.com>
2014-07-17 01:49:43 -0400

    Added _config.js.

---
+++ scripts/_config.js
@@ -0,0 +1,8 @@
+var GITHUB_API_BASE_URL = 'https://api.github.com';
+
+var REPO_OWNER = 'chessmasterhong';
+
+var CLIENT = {
+    ID: 'YOUR_GITHUB_API_CLIENT_ID',
+    SECRET: 'YOUR_GITHUB_API_CLIENT_SECRET'
+}
