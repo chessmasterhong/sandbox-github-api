75ec796d48d725482c9d5379cb9eb36dd9fe792a
Kevin Chan <chessmasterhong@yahoo.com>
2014-07-16 21:05:57 -0400

    Refactored get contributors script to utils.js.

--- scripts/get_contributors.js
+++ scripts/get_contributors.js
@@ -1,7 +1,3 @@
 var REPO_NAME = 'WaterEmblem';
 
-var data = httpGet(GITHUB_API_BASE_URL + '/repos/' + REPO_OWNER + '/' + REPO_NAME + '/contributors?client_id=' + CLIENT.ID + '&client_secret=' + CLIENT.SECRET);
-
-for(var contributor_index = 0; contributor_index < data.length; contributor_index++) {
-    document.write(data[contributor_index].login + '<br>');
-}
+document.write(getContributors(REPO_NAME, CLIENT.ID, CLIENT.SECRET));
--- scripts/utils.js
+++ scripts/utils.js
@@ -9,3 +9,21 @@ function httpGet(url, json_output) {
 
     return json_output === false ? xmlHttp.responseText : JSON.parse(xmlHttp.responseText);
 }
+
+function getContributors(repo_name, client_id, client_secret) {
+    var contributors = [];
+
+    var url = GITHUB_API_BASE_URL + '/repos/' + REPO_OWNER + '/' + REPO_NAME + '/contributors'
+
+    if(typeof client_id !== 'undefined' && typeof client_secret !== 'undefined') {
+        url += '?client_id=' + client_id + '&client_secret=' + client_secret;
+    }
+
+    var data = httpGet(url);
+    
+    for(var contributor_index = 0; contributor_index < data.length; contributor_index++) {
+        contributors.push(data[contributor_index].login);
+    }
+
+    return contributors;
+}
