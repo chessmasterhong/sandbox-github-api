f9a9988b29946468d83828a84797209521d37845
Kevin Chan <chessmasterhong@yahoo.com>
2014-12-19 15:25:23 -0500

    Removed repo contributors module.

--- scripts/app.js
+++ scripts/app.js
@@ -10,47 +10,7 @@ var app = angular.module('app', [
     $stateProvider
         .state('home', {
             url: '',
-            views: {
-                '': {
-                    controller: 'homeController',
-                    templateUrl: 'partials/home.partial.html',
-                },
-                'contributors': {
-                    controller: 'contributorsController',
-                    templateUrl: 'partials/contributors.partial.html'
-                }
-            }
-        })
-        .state('comments', {
-            url: '/comments'
-            //controller: 'commentsController',
-            //templateUrl: 'partials/comments.partial.html'
-        })
-        .state('commits', {
-            url: '/commits'
-            //controller: 'commitsController',
-            //templateUrl: 'partials/commits.partial.html'
-        })
-        .state('issues', {
-            url: '/issues'
-            //controller: 'issuesController',
-            //templateUrl: 'partials/issues.partial.html'
-        })
-        .state('pulls', {
-            url: '/pulls'
-            //controller: 'pullsController',
-            //templateUrl: 'partials/pulls.partial.html'
-        })
-        .state('lines', {
-            url: '/lines'
-            //controller: 'linesController',
-            //templateUrl: 'partials/lines.partial.html'
+            controller: 'homeController',
+            templateUrl: 'partials/home.partial.html'
         });
-})
-.run(function($rootScope) {
-    'use strict';
-
-    $rootScope.$on('foundRepo', function(event, data) {
-        $rootScope.$broadcast('foundRepo', data);
-    });
 });
--- scripts/controllers/contributorsController.js
+++
@@ -1,9 +0,0 @@
-app.controller('contributorsController', function($scope, githubAPIFactory, appService) {
-    'use strict';
-
-    $scope.contributors = 'None';
-
-    $scope.$on('foundRepo', function(event, data) {
-        $scope.contributors = appService.getUser();
-    });
-});
