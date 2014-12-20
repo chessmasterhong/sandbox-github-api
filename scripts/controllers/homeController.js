b31b2c8d28f4a9b645e796699c40093f4e95bb95
Kevin Chan <chessmasterhong@yahoo.com>
2014-12-19 20:43:03 -0500

    Merged repo contributors module with query module.

--- scripts/controllers/homeController.js
+++ scripts/controllers/homeController.js
@@ -1,26 +1,33 @@
 app.controller('homeController', function($scope, githubAPIFactory, appService) {
     'use strict';
 
+    $scope.foundRepo = false;
+
+    $scope.user = '';
+    $scope.repo = '';
+
     $scope.submitQuery = function() {
         githubAPIFactory.getUser($scope.user, CLIENT)
             .success(function(user) {
                 githubAPIFactory.getRepos(user.login, CLIENT)
                     .success(function(repos) {
-                        var foundRepo = false;
 
                         for(var i = 0; i < repos.length; i++) {
                             if(repos[i].name.toLowerCase() === $scope.repo.toLowerCase()) {
                                 appService.setUser(user.login);
                                 appService.setRepo(repos[i].name);
-                                foundRepo = true;
+
+                                $scope.user = user.login;
+                                $scope.repo = repos[i].name;
+
+                                $scope.foundRepo = true;
+                                generateReport();
                                 break;
                             }
                         }
 
-                        if(!foundRepo) {
+                        if($scope.foundRepo === false) {
                             console.log('Cannot find repository name.');
-                        } else {
-                            $scope.$emit('foundRepo', foundRepo);
                         }
                     });
             })
@@ -28,4 +35,13 @@ app.controller('homeController', function($scope, githubAPIFactory, appService) 
                 console.log('Cannot find user name.');
             });
     };
+
+    function generateReport() {
+        $scope.contributors = [];
+
+        githubAPIFactory.getContributors(appService.getUser(), appService.getRepo(), CLIENT)
+            .success(function(contributors) {
+                $scope.contributors = contributors;
+            });
+    }
 });
--- scripts/factories/githubAPIFactory.js
+++ scripts/factories/githubAPIFactory.js
@@ -16,6 +16,14 @@ app.factory('githubAPIFactory', function($http) {
                 url: 'https://api.github.com/users/' + user + '/repos?client_id=' + githubAPI.id + '&client_secret=' + githubAPI.secret,
                 json: true
             });
+        },
+
+        getContributors: function(user, repo, githubAPI) {
+            return $http({
+                method: 'GET',
+                url: 'https://api.github.com/repos/' + user + '/' + repo + '/contributors?client_id=' + githubAPI.id + '&client_secret=' + githubAPI.secret,
+                json: true
+            });
         }
     };
 });
