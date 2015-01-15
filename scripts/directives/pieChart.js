1fe3b5317c9cdf321cf5b6e1fb9723031d6d6052
Kevin Chan <chessmasterhong@yahoo.com>
2015-01-14 19:25:54 -0500

    Added commits pie chart to reports view.

--- scripts/directives/pieChart.js
+++ scripts/directives/pieChart.js
@@ -7,48 +7,57 @@ app.directive('pieChart', function($window) {
         link: function(scope, element, attributes) {
             scope.$watch('values', function(values) {
                 if(values) {
-                    var val = [];
-                    angular.forEach(values, function(v) {
-                        val.push(v.commits);
-                    });
+                    //var val = [];
+                    //angular.forEach(values, function(v) {
+                    //    val.push(v.commits);
+                    //});
 
                     var width = 400;
                     var height = 400;
                     var radius = Math.min(width, height) / 2;
                     var color = $window.d3.scale.category20c();
 
-                    var arc = $window.d3.svg.arc()
-                        .outerRadius(radius - 10)
-                        .innerRadius(0);
-
-                    var pie = $window.d3.layout.pie()
-                        .sort(null)
-                        .value(function(d) { return d.commits; });
-
                     var svg = $window.d3.select('body')
                         .append('svg:svg')
-                            //.data([values])
+                            .data([values])
                             .attr('width', width)
                             .attr('height', height)
                         .append('svg:g')
                             .attr('transform', 'translate(' + radius + ',' + radius + ')');
 
-                    var g = svg.selectAll('.arc')
-                            .data(pie(val))
-                        .enter().append('g')
-                            .attr('class', 'arc');
+                    var pie = $window.d3.layout.pie()
+                        .sort(null)
+                        .value(function(d) { return d.commits; });
 
-                    g.append('path')
-                        .attr('d', arc)
-                        .style('fill', function(d) { return color(d.data.commits); });
+                    var arc = $window.d3.svg.arc()
+                        //.innerRadius(0)
+                        .outerRadius(radius);
 
-                    g.append('text')
-                        .attr('transform', function(d) { return 'translate(' + radius + ',' + radius + ')'; })
-                        .attr('dy', '0.35em')
-                        .style('text-anchor', 'middle')
-                        .text(function(d) { return d.data.commits; });
+                    var arcs = svg.selectAll('g.slice')
+                        .data(pie)
+                        .enter().append('svg:g')
+                            .attr('class', 'slice');
+
+                    arcs.append('svg:path')
+                        .attr('fill', function(d, i) {
+                            return color(i);
+                        })
+                        .attr('d', function(d) {
+                            return arc(d);
+                        });
+
+                    arcs.append('svg:text')
+                        .attr('transform', function(d) {
+                            d.innerRadius = 0;
+                            d.outerRadius = radius;
+                            return 'translate(' + arc.centroid(d) + ')';
+                        })
+                        .attr('text-anchor', 'middle')
+                        .text(function(d, i) {
+                            return values[i].name;
+                        });
                 }
-            });
+            }, true);
         }
     };
 });
